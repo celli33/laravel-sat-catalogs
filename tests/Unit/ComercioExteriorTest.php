927a6f9d4994d0a459c623f93c316158c11567f0
celli33 <limon.martinez.misael@gmail.com>
2022-03-15 22:11:40 -0600

    fix: fracción arancelaria

--- tests/Unit/ComercioExteriorTest.php
+++ tests/Unit/ComercioExteriorTest.php
@@ -58,7 +58,7 @@ class ComercioExteriorTest extends TestCase
             'vigencia_hasta',
             'unidad',
         ];
-        $this->assertTableNamePropertiesAndCount(CceFraccionArancelaria::class, 'cce_fracciones_arancelarias', $expectedKeys, 24617);
+        $this->assertTableNamePropertiesAndCount(CceFraccionArancelaria::class, 'cce_fracciones_arancelarias', $expectedKeys, 2462);
     }
 
     public function test_incoterms(): void
